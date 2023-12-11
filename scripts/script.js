87dc0c8695b12162dcd51ec06be48ebc29b8d384
KayGV25 <85599037+KayGV25@users.noreply.github.com>
2023-12-11 13:08:54 +0700

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+function clock(){
+    let date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    if(sec < 10) sec = '0' + sec;
+    if(min < 10) sec = '0' + min;
+    if(hour < 10) sec = '0' + hour;
+    document.getElementById("clock").innerHTML = `${hour} : ${min} : ${sec}`;
+}
+setInterval(clock,1000);
