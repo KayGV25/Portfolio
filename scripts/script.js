6756048d8099246a80f20bbd09ade3709bef6d98
KayGV25 <khuongvudang25@gmail.com>
2023-12-12 07:34:27 +0700

    change clock

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,8 @@ function clock(){
     var min = date.getMinutes();
     var sec = date.getSeconds();
     if(sec < 10) sec = '0' + sec;
-    if(min < 10) sec = '0' + min;
-    if(hour < 10) sec = '0' + hour;
+    if(min < 10) min = '0' + min;
+    if(hour < 10) hour = '0' + hour;
     document.getElementById("clock").innerHTML = `${hour} : ${min} : ${sec}`;
 }
 setInterval(clock,1000);
