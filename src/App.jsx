256ca40761f1ab04a90f3f23410d56b515303274
KayGV <khuongvudang25@gmail.com>
2024-09-02 00:56:54 +0700

    [ADD] Weather

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./sections/Home";
 import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
 import ThemeToggle from "./components/ThemeToggle";
+import Weather from "./components/Weather"
 
 export default function App() {
   const checkMobile = isMobile()
@@ -21,26 +22,22 @@ export default function App() {
   const [homeVisible, setHomeVisible] = useState(homeRefVisible)
   const [projectVisible, setProjectVisible] = useState(projecReftsVisible)
   const [contactVisible, setContactVisible] = useState(contactRefVisible)
-  const [isVisible, setIsVisible] = useState(false)
 
   useEffect(() => {
     if (homeRefVisible) {
       setHomeVisible(true)
       setProjectVisible(false)
       setContactVisible(false)
-      setIsVisible(false)
     }
     if (projecReftsVisible) {
       setProjectVisible(true)
       setHomeVisible(false)
       setContactVisible(false)
-      setIsVisible(true)
     }
     if (contactRefVisible) {
       setContactVisible(true)
       setHomeVisible(false)
       setProjectVisible(false)
-      setIsVisible(true)
     }
   })
 
@@ -54,6 +51,7 @@ export default function App() {
         <div className={`fixed top-7 left-32 transition-all duration-200 ${!homeVisible ? "opacity-100" : "opacity-0"}`}>
           <p className="text-3xl">Vũ Đăng Khương</p>
           <p className="text-xl font-bold">Software Engineer</p>
+          <Weather/>
         </div>
       }
       <main className="flex flex-col gap-72">
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,3 +1,66 @@
+import { useState, useEffect } from "react"
+
 export default function Weather(){
+    const weatherAPIKey = import.meta.env.VITE_WEATHER_API_KEY
+    const weatherAPIBaseURL = import.meta.env.VITE_WEATHER_API_BASE_URL
+
+    const [weather, setWeather] = useState({
+        "location": {
+            "name": "Ho Chi Minh City",
+            "region": "",
+            "country": "Vietnam",
+            "lat": 10.75,
+            "lon": 106.67,
+            "tz_id": "Asia/Ho_Chi_Minh",
+            "localtime_epoch": 1725210322,
+            "localtime": "2024-09-02 00:05"
+        },
+        "current": {
+            "last_updated_epoch": 1725210000,
+            "last_updated": "2024-09-02 00:00",
+            "temp_c": 24.2,
+            "is_day": 0,
+            "condition": {
+                "text": "Light rain",
+                "icon": "//cdn.weatherapi.com/weather/64x64/night/296.png",
+                "code": 1183
+            },
+            "precip_mm": 2.22,
+            "humidity": 100,
+            "cloud": 25,
+            "feelslike_c": 26.8,
+            "feelslike_f": 80.2,
+            "air_quality": {
+                "co": 2162.9,
+                "no2": 33.2,
+                "o3": 0.0,
+                "so2": 39.6,
+                "pm2_5": 94.3,
+                "pm10": 107.6,
+                "us-epa-index": 4,
+                "gb-defra-index": 10
+            }
+        }
+    })
     
+    useEffect(() => {
+        let latlong = "";
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                latlong = position.coords.latitude.toString() + "," + position.coords.longitude.toString();
+
+                fetch(`${weatherAPIBaseURL}/current.json?key=${weatherAPIKey}&q=${latlong}`)
+                .then(res => res.json())
+                .then(res => setWeather(res))
+            });
+        }
+    }, []);
+    
+
+    return(
+        <div className="dark:bg-zinc-50 bg-zinc-800 dark:text-zinc-800 text-zinc-50 w-fit px-5 py-2 flex justify-center rounded-3xl gap-2 mt-4">
+            <img src={weather.current.condition.icon} alt={weather.current.condition.text} className="size-7"/>
+            <p className="my-auto">{weather.current.temp_c}&deg;C</p>
+        </div>
+    )
 }
