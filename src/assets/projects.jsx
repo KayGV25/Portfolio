11669954e49daf44ece3ab01d02be0a598d9a120
KayGV <khuongvudang25@gmail.com>
2024-09-01 23:32:58 +0700

    [UPDATE] position of projects

--- src/assets/projects.jsx
+++ src/assets/projects.jsx
@@ -1,12 +1,5 @@
 export const projects = [
     {
-        "pName": "Streaming Service",
-        "role": "Project Manager, Fullstack",
-        "description": "Developed an online movie streaming website with features such as movie search, saving movies to favorites, and payment processing.",
-        "time": "10/2023 - 12/2023",
-        "links": ["https://github.com/KayGV25/UIT_WEB_BCUK18_23560026_23560028"]
-    },
-    {
         "pName": "ESRO",
         "role": "Project Manager, Frontend",
         "description": "Created a community-driven recipe website based on user contributions. Features include account creation, saving favorite recipes, posting recipes, and searching for recipes by name, tags, and ingredients.",
@@ -14,6 +7,13 @@ export const projects = [
         "links": ["https://uit-web2-frontend.vercel.app",
         "https://github.com/KayGV25/UIT_Web2-Frontend",
         "https://github.com/KayGV25/UIT_Web2-Backend"]
+    },
+    {
+        "pName": "Streaming Service",
+        "role": "Project Manager, Fullstack",
+        "description": "Developed an online movie streaming website with features such as movie search, saving movies to favorites, and payment processing.",
+        "time": "10/2023 - 12/2023",
+        "links": ["https://github.com/KayGV25/UIT_WEB_BCUK18_23560026_23560028"]
     }
 ]
 
